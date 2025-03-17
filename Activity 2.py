56bfc136c6baddad2a89679c959ae37962abc01c
Shaurya094 <Shauryauprari094@gmail.com>
2025-03-17 17:25:41 +1100

    Loops

---
+++ Activity 2.py
@@ -0,0 +1,7 @@
+string = input("Enter the word you want to reverse.")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print ("\nBefore: ", string)
+print ("Afer: ", string2)
---
+++ Activity 3.py
@@ -0,0 +1,4 @@
+x = int(input("Give number:"))
+print ("Numbers from {0} to {1} are: ")
+for i in range (x,0,-1000):
+    print (i)
